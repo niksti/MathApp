743fc39596033f96be380fef9cef0e2c390c3013
Nikos Stivaktakis <niksti@gmx.net>
2016-05-24 19:07:47 +0200

    second commit

---
+++ MathApp Xcode/MathApp XcodeTests/MathApp_XcodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  MathApp_XcodeTests.swift
+//  MathApp XcodeTests
+//
+//  Created by Nikos Stivaktakis on 24.05.16.
+//  Copyright © 2016 Nikos Stivaktakis. All rights reserved.
+//
+
+import XCTest
+@testable import MathApp_Xcode
+
+class MathApp_XcodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
